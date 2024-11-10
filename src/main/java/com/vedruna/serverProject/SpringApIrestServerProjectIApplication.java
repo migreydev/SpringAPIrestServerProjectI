84fde1e6a808e549ab1dd831eb75a2d95156c428
Miguel Ángel Reyes Méndez <49988347+migreydev@users.noreply.github.com>
2024-11-10 17:08:11 +0100

    añadiendo trabajo evaluacion I servidor

---
+++ src/main/java/com/vedruna/serverProject/SpringApIrestServerProjectIApplication.java
@@ -0,0 +1,13 @@
+package com.vedruna.serverProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApIrestServerProjectIApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApIrestServerProjectIApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vedruna/serverProject/SpringApIrestServerProjectIApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vedruna.serverProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApIrestServerProjectIApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
