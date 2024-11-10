2bd4a3df64031aaa9ea7c2ea455334b7b0eff8cd
Miguel Ángel Reyes Méndez <49988347+migreydev@users.noreply.github.com>
2024-11-10 20:07:58 +0100

    relación completa

--- src/main/java/com/vedruna/serverProject/persistance/model/Project.java
+++ src/main/java/com/vedruna/serverProject/persistance/model/Project.java
@@ -1,12 +1,17 @@
 package com.vedruna.serverProject.persistance.model;
 
 import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -42,6 +47,14 @@ public class Project {
 	
 	@Column(name="picture")
 	private String picture;
+	
+	@ManyToOne
+    @JoinColumn(name = "status_status_id")
+    private Status status;
+	
+	@ManyToMany(mappedBy="ProjectsHasDevelopers")
+	private List<Developer>developersHasProjects = new ArrayList<>();
+	
 	
 
 }
--- src/main/java/com/vedruna/serverProject/persistance/model/Status.java
+++ src/main/java/com/vedruna/serverProject/persistance/model/Status.java
@@ -1,11 +1,15 @@
 package com.vedruna.serverProject.persistance.model;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -23,5 +27,8 @@ public class Status {
 	
 	@Column(name="status_name")
 	private String status_name;
+	
+	@OneToMany(mappedBy = "status")
+	private List<Project>projects = new ArrayList<>();
 
 }
