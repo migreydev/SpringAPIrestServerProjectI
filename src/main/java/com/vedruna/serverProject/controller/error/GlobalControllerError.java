18405f74996cf7b56d9b37ba1907d60d743069cd
Miguel Ángel Reyes Méndez <49988347+migreydev@users.noreply.github.com>
2024-11-12 17:59:43 +0100

    guardar proyecto y validaciones

--- src/main/java/com/vedruna/serverProject/controller/error/GlobalControllerError.java
+++ src/main/java/com/vedruna/serverProject/controller/error/GlobalControllerError.java
@@ -6,33 +6,35 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.vedruna.serverProject.exceptions.ExceptionErrorPage;
+import com.vedruna.serverProject.exceptions.ExceptionInvalidProjectData;
 import com.vedruna.serverProject.exceptions.ExceptionPageNotFound;
-import com.vedruna.serverProject.exceptions.ExceptionProjectNotFound;
 import com.vedruna.serverProject.persistance.model.ApiError;
 
 @RestControllerAdvice
 public class GlobalControllerError {
 	
-	// Maneja la excepción cuando una página no es encontrada.
+	// Maneja la excepción cuando una página no es encontrada
 	@ExceptionHandler(ExceptionPageNotFound.class)
 	public ResponseEntity<ApiError> pageNotFound(ExceptionPageNotFound e) {
 		 ApiError apiError = new ApiError(HttpStatus.BAD_REQUEST, e.getMessage());
 		 return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(apiError);
 	}
 	
-	// Maneja la excepción de error en la página.
+	// Maneja la excepción de error en la página
 	@ExceptionHandler(ExceptionErrorPage.class)
 	public ResponseEntity<ApiError> handleErrorPageException(ExceptionErrorPage e) {
 	    ApiError apiError = new ApiError(HttpStatus.BAD_REQUEST, e.getMessage());
 	    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(apiError);
 	}
 	
-	// Maneja la excepción cuando no se encuentra un proyecto en la base de datos.
-	@ExceptionHandler(ExceptionProjectNotFound.class)
-	public ResponseEntity<ApiError> projectNotFound(ExceptionProjectNotFound e) {
-		 ApiError apiError = new ApiError(HttpStatus.BAD_REQUEST, e.getMessage());
-		 return ResponseEntity.status(HttpStatus.NOT_FOUND).body(apiError);
+	//Maneja la excepción cuando los datos del proyecto no son válidos
+	 @ExceptionHandler(ExceptionInvalidProjectData.class)
+	 public ResponseEntity<ApiError> invalidProjectData(ExceptionInvalidProjectData e) {
+	     ApiError apiError = new ApiError(HttpStatus.BAD_REQUEST, e.getMessage());
+	     return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(apiError);
 	}
+	
+	
 
 
 }
--- src/main/java/com/vedruna/serverProject/services/project/ProjectServiceI.java
+++ src/main/java/com/vedruna/serverProject/services/project/ProjectServiceI.java
@@ -5,6 +5,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 
 import com.vedruna.serverProject.dto.ProjectDTO;
+import com.vedruna.serverProject.persistance.model.ApiResponse;
+import com.vedruna.serverProject.persistance.model.Project;
 
 public interface ProjectServiceI {
 	
@@ -13,6 +15,9 @@ public interface ProjectServiceI {
 	
 	// Método para obtener un proyecto por su nombre, devolviendo un ResponseEntity
 	ResponseEntity<ProjectDTO> getProjectByName(String name);
+	
+	//Método para agregar un project, devolviendo un ResponseEntity
+	ResponseEntity<ApiResponse<Project>>addProject(Project project);
 	
 	
 }
